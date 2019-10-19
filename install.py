e0d47e1423e0f687686ca46d8d98cc9969ae802d
oggy3012 <55061770+oggy3012@users.noreply.github.com>
2019-10-19 17:55:45 +0530

    Add files via upload

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import os,time
 time.sleep(5)
 print("Press Enter")
-os.system("pip install --upgrade pip&&pip install requests&&cd&&cd TBomb&&chmod +x TBomb.sh && ./TBomb.sh")
+os.system("pip install --upgrade pip&&pip install requests&&cd&&cd TBomb&&chmod +x TBomb.sh &&cd&&cd TBomb-Infinity && python3 TBomb-Infinity.py")
