320147c86811303f3f2f76d1d46024148a064241
oggy3012 <55061770+oggy3012@users.noreply.github.com>
2019-10-19 15:40:37 +0530

    Add files via upload

--- install.py
+++ install.py
@@ -1,7 +1,4 @@
-import os
-a=int(input("Press 1 for Termux\nPress 2 For Linux\n->>>>  "))
-if a==1:
-  os.system("pkg install python3&&pip install --upgrade pip&&pip install requests&&cd&&cd TBomb-Infinity&&chmod +x TBomb-Infinite.py && python3 TBomb-Infinite.py")
-else:
-  os.system("apt install python3&&pip install --upgrade pip&&pip install requests&&cd&&cd TBomb-Infinity&&chmod +x TBomb-Infinite.py && python3 TBomb-Infinite.py")
-
+import os,time
+time.sleep(5)
+print("Press Enter")
+os.system("pip install --upgrade pip&&pip install requests&&cd&&cd TBomb&&chmod +x TBomb.sh && ./TBomb.sh")
