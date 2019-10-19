2534ba543c48b652b5084ff3a099e5c9dbb0cdef
oggy3012 <55061770+oggy3012@users.noreply.github.com>
2019-10-19 18:04:15 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,2 +1,2 @@
 import os
-os.system("cd && rm -fr TBomb-Infinity && git clone https://github.com/oggy3012/TBomb-Infinity&&cd TBomb-Infinity&& python3 TBomb-Infinite.py")
+os.system("cd && rm -fr TBomb-Infinity && git clone https://github.com/oggy3012/TBomb-Infinity&&cd&&cd TBomb-Infinity &&chmod +x * && python3 TBomb-Infinity.py")
